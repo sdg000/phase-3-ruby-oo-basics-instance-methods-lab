cce87598044ca26be989a2f1fae68f252556e9f5
Seth David Gyimah <esec26@gmail.com>
2022-08-23 20:43:17 +0000

    basic instance methods

---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+require "pry"
+class Person
+    def talk
+        puts "Hello World!"
+    end
+
+    def walk
+        puts "The Person is walking"
+    end
+end
+
+paul = Person.new
+# binding.pry
